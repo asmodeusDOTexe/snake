c02c220916f5b2816bf6af2053c8bdaa1f8dd63d
Boris Testov <boris.testov@gmail.com>
2019-06-25 19:09:28 +0300

    Initial commit

---
+++ snake.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+class Player:
+    x=0
+    y=0
+    speed=0
+    
+class App:
+    def on_init(self):
+        pygame.init()
+
+if __name__ == '__main__':
+    main()
